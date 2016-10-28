baf8f3bc3f5ac7da51e8a6b2ceb551120116519a
dotsoftware <der.marty@gmail.com>
2016-10-28 15:03:07 +0200

    added img err-src directive

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,17 @@
 angular.module('app.directives', [])
 
+.directive('err-src', function() {
+  return {
+    link: function(scope, element, attrs) {
+      element.bind('error', function() {
+        if (attrs.src != attrs.errSrc) {
+          attrs.$set('src', attrs.errSrc);
+        }
+      });
+    }
+  }
+})
+  
 .directive('blankDirective', [function(){
 
 }]);
